9ae6fee8313e9c44c15ebc6557f1bc72049f98f5
Michael Lyons <michael@fireside21.com>
2014-02-01 11:49:21 -0500

    Icon, SOP app structure, cleanup

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 RadioPebbledise
 ===============
 
-Pebble watch control for RadioParadise Android App
+This is ultimately intended to be a full-featured [Pebble](http://getpebble.com) watch control for [RadioParadise](http://radioparadise.com)'s [Android app](https://play.google.com/store/apps/details?id=com.earthflare.anddroid.radioparadisewidget&hl=en).
+
+Its development is not currently sanctioned, but I'm hoping things work out.
+
+Development Notes
+=================
+
+Don't laugh. This is my first time with Pebble or with straight-up C. The below is bound to change as I learn how to do things.
+
+(Intended) Structure
+--------------------
+
++ *Window* Startup screen
+    + *Layer* Layer
+        + *Image* RP Logo
++ *Window* Main (Now Playing) screen
+    + *Layer* Controls
+        + *Image* Sprites
+    + *Layer* Song info
+        + *Text* Song name
+        + *Text* Band name
++ *Window*? Ratings screen
+    + *MenuLayer* Ratings
--- src/radio_pebbledise.c
+++ src/radio_pebbledise.c
@@ -1,46 +1,67 @@
-/*
-
-   Demonstrate how to display a two color, black and white bitmap
-   image with no transparency.
-
-   The original source image is from:
-
-      <http://openclipart.org/detail/26728/aiga-litter-disposal-by-anonymous>
-
-   The source image was converted from an SVG into a RGB bitmap using
-   Inkscape. It has no transparency and uses only black and white as
-   colors.
-
- */
-
 #include "pebble.h"
 
-static Window *window;
+/*
+ * This is ultimately intended to be a full-featured control
+ * for RadioParadise's Android app.
+ * https://play.google.com/store/apps/details?id=com.earthflare.anddroid.radioparadisewidget&hl=en
+ * 
+ * Its development is not currently sanctioned by RP.
+ *
+ */
 
-static BitmapLayer *image_layer;
+
+
+char *ratings[] = {
+  "- None -",
+  "10 - Godlike",
+  "9 - Outstanding",
+  "8 - Most Excellent",
+  "7 - Quite Likeable",
+  "6 - Pretty Good",
+  "5 - Decent",
+  "4 - Acceptable",
+  "3 - Ho Hum",
+  "2 - Marginal",
+  "1 - Sucko-barfo"
+};
+#define NUM_RATINGS sizeof(ratings) / sizeof(char*)
+
+/*
+ * Global and static UI elements.
+ *
+ * This app uses two windows:
+ * - the main_window
+ * - the rating_window, which uses a menu_layer to rate a song
+ *
+ */
+
+static Window *main_window;
 
 static GBitmap *image;
+static BitmapLayer *image_layer;
 
-int main(void) {
-  window = window_create();
-  window_stack_push(window, true /* Animated */);
 
-  Layer *window_layer = window_get_root_layer(window);
+static void init() {
+  main_window = window_create();
+  window_stack_push(main_window, true /* Animated */);
+
+  image = gbitmap_create_with_resource(IMAGE_NO_LITTER);
+
+  Layer* window_layer = window_get_root_layer(main_window);
   GRect bounds = layer_get_frame(window_layer);
-
-  // This needs to be deinited on app exit which is when the event loop ends
-  image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NO_LITTER);
-
-  // The bitmap layer holds the image for display
   image_layer = bitmap_layer_create(bounds);
   bitmap_layer_set_bitmap(image_layer, image);
-  bitmap_layer_set_alignment(image_layer, GAlignCenter);
   layer_add_child(window_layer, bitmap_layer_get_layer(image_layer));
+}
 
-  app_event_loop();
-
+static void deinit() {
   gbitmap_destroy(image);
-
   bitmap_layer_destroy(image_layer);
-  window_destroy(window);
+  window_destroy(main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
 }

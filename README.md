48014ff446fda73e2bbae1ed5f5f94b6390abb34
Michael Lyons <michael@fireside21.com>
2014-02-01 14:58:34 -0500

    Load up an ActionBarLayer

--- README.md
+++ README.md
@@ -13,17 +13,29 @@ Don't laugh. This is my first time with Pebble or with straight-up C. The below 
 (Intended) Structure
 --------------------
 
-+ __Window__ Startup screen
-    + __Layer__ Layer
-        + __Image__ RP Logo
 + __Window__ Main (Now Playing) screen
-    + __Layer__ Controls
-        + __Image__ Sprites
+    + __ActionBarLayer__ Controls
+        1. Return to Stream (if PSD)
+           + If not PSD, rate song?
+           + Long Press to rate song when PSD?
+        2. Play/Stop
+        3. PSD
     + __Layer__ Song info
-        + __Text__ Song name
-        + __Text__ Band name
+        + __TextLayer__ Song name
+        + __TextLayer__ Band name
 + __Window__? Ratings screen
+    + __TextLayer__? Song name?
     + __MenuLayer__ Ratings
+        + It'll be interesting to try to load current ratings and hide the "None" option if a song rating already exists.
+
+Possible Future Features
+------------------------
+
++ __Window__ Startup screen
+    + __ImageLayer__ Layer
+        + __Image__ RP Logo
++ __Window__ Lyrics screen
+    + __ScrollLayer__ Lyrics container
 
 [pebble]: http://getpebble.com "Official Pebble Website"
 [rp]: http://radioparadise.com
--- src/radio_pebbledise.c
+++ src/radio_pebbledise.c
@@ -38,16 +38,29 @@ bool IS_PLAYING = false;
 
 static Window *main_window;
 static Window *rating_window;
+static ActionBar *main_action_bar;
 
-static GBitmap *image;
+static GBitmap *icon_ctrl_play;
+static GBitmap *icon_ctrl_stop;
+static GBitmap *icon_ctrl_psd;
+static GBitmap *icon_ctrl_return;
+static GBitmap *test_image;
 static BitmapLayer *image_layer;
 
 /*
  * Event handlers
  */
 void play_pause_click_handler(ClickRecognizerRef recognizer, void *context){
-    image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RP_LOGO_MINI);
-    bitmap_layer_set_bitmap(image_layer, image);
+    IS_PLAYING = !IS_PLAYING;
+    test_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RP_LOGO_MINI);
+    bitmap_layer_set_bitmap(image_layer, test_image);
+
+    if(IS_PLAYING){
+        action_bar_layer_set_icon(main_action_bar, BUTTON_ID_SELECT, icon_ctrl_stop);
+    }
+    else{
+        action_bar_layer_set_icon(main_action_bar, BUTTON_ID_SELECT, icon_ctrl_play);
+    }
 }
 
 void main_config_provider(void *context){
@@ -61,15 +74,26 @@ static void init() {
     main_window = window_create();
     window_stack_push(main_window, true /* Animated */);
 
-    image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NO_LITTER);
+    main_action_bar = action_bar_layer_create();
+    action_bar_layer_add_to_window(main_action_bar, main_window);
+    action_bar_layer_set_click_config_provider(main_action_bar, main_config_provider);
+
+    icon_ctrl_play = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CTRL_PLAY);
+    icon_ctrl_stop = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CTRL_STOP);
+    icon_ctrl_psd = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CTRL_PSD);
+    icon_ctrl_return = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CTRL_RETURN);
+
+    action_bar_layer_set_icon(main_action_bar, BUTTON_ID_UP, icon_ctrl_return);
+    action_bar_layer_set_icon(main_action_bar, BUTTON_ID_DOWN, icon_ctrl_psd);
+    action_bar_layer_set_icon(main_action_bar, BUTTON_ID_SELECT, icon_ctrl_play);
+
+    test_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NO_LITTER);
 
     Layer* window_layer = window_get_root_layer(main_window);
     GRect bounds = layer_get_frame(window_layer);
     image_layer = bitmap_layer_create(bounds);
-    bitmap_layer_set_bitmap(image_layer, image);
+    bitmap_layer_set_bitmap(image_layer, test_image);
     layer_add_child(window_layer, bitmap_layer_get_layer(image_layer));
-
-    window_set_click_config_provider(main_window, main_config_provider);
 }
 
 static void deinit() {
